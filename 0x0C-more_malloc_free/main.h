e671424e05db7b01c1aa1f346a6c56df95351dd0
Mohammed <ss1366242@gmail.com>
2023-03-16 13:37:50 -0700

    betty 101-mul.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- *
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0B-more_malloc_free directory.
- */
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
